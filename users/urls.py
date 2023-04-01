44fee0a47b958299a7dd5a470e780ed611f5908e
andytwoods <andytwoods@gmail.com>
2023-04-01 12:04:52 +0100

    purple bootstrap theme

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
     path("login/", views.MyLoginView.as_view(), name='login'),
+    path("privacy-policy/", TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
+    path("terms-and-conditions/", TemplateView.as_view(template_name='terms_and_conditions.html'), name='terms_and_conditions'),
 ]
