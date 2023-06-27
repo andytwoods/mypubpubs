0a5fe245eb49b23b6c3fd1b982fd1a282aafb9cc
andytwoods <andytwoods@gmail.com>
2023-06-27 20:10:12 +0100

    upload photo directly

--- graffiti/forms.py
+++ graffiti/forms.py
@@ -1,18 +1,15 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Column, Row, Submit
+from crispy_forms.layout import Layout, Column, Row, Submit, Field
 from django import forms
-
-from graffiti.models import GraffitiImage
-
 
 class UploadImageForm(forms.Form):
     image = forms.ImageField()
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        self.fields['image'].label = ''
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Row(Column("image", css_class='col-md-6')),
+            Row(Column(Field("image", accept='image/*;capture=camera'), css_class='col-md-6')),
             Row(
                 Column(
                     Submit("submit", "Upload", css_class="my-4 btn-lg"),
